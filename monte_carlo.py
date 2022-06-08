66c5cddf5a4d7a2a554e9d27ea2410e7d59f1e47
Joseph McLaughlin <cewmjcm@gmail.com>
2022-06-08 10:59:05 -0700

    Added type and size tests for mc.sample

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,6 +1,14 @@
+import numpy as np
+
 
 class MonteCarlo():
 
     def __init__(self, model, theta_distribution): 
         self.model = model 
         self.theta_distribution = theta_distribution
+
+    def sample(self, num_samples):
+        result = np.zeros(num_samples)
+        
+
+        return result
--- test_monte_carlo.py
+++ test_monte_carlo.py
@@ -3,7 +3,7 @@ from monte_carlo import MonteCarlo
 
 
 
-def test_init_monte_carlo(mocker):
+def test_monte_carlo_init(mocker):
     model = mocker.Mock()
     theta_distribution = mocker.Mock()
 
@@ -11,3 +11,24 @@ def test_init_monte_carlo(mocker):
 
     assert mc.model == model
     assert mc.theta_distribution == theta_distribution
+
+@pytest.mark.parametrize('num_samples', (100, 1000))
+def test_monte_carlo_sample_type(mocker, num_samples): 
+    import numpy as np
+    model = mocker.Mock()
+    theta_distribution = mocker.Mock()
+
+    mc = MonteCarlo(model, theta_distribution)
+    samples = mc.sample(num_samples)
+
+    assert isinstance(samples, np.ndarray)
+
+@pytest.mark.parametrize('num_samples', range(0, 100))
+def test_monte_carlo_sample_type(mocker, num_samples): 
+    model = mocker.Mock()
+    theta_distribution = mocker.Mock()
+
+    mc = MonteCarlo(model, theta_distribution)
+    samples = mc.sample(num_samples)
+
+    assert samples.size == num_samples
