ecd8ba970c1674a457e262023b53f0dfbf0ed1eb
David Randall <madmathskills@gmail.com>
2022-06-08 10:31:41 -0600

    added evaluate_distance and train_surrogate

--- surrogate_model.py
+++ surrogate_model.py
@@ -1,3 +1,10 @@
 class SurrogateModel:
     def __init__(self, method):
         self.method = method
+
+    def evaluate_distance(self, theta):
+        return float(self.method.predict(theta))
+
+    def train_surrogate(self, hdf5_file):
+        self.method.fit(hdf5_file['\/']['explicit_model']['theta'], hdf5_file['\/']['explicit_model']['distance'])
+
