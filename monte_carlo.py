1a058dc89b4e34d8439f1f793ec2eb678a285598
Joseph McLaughlin <cewmjcm@gmail.com>
2022-06-08 09:30:18 -0700

    add mc init test

---
+++ monte_carlo.py
@@ -0,0 +1,6 @@
+
+class MonteCarlo():
+
+    def __init__(self, model, theta_distribution): 
+        self.model = model 
+        self.theta_distribution = theta_distribution
--- test_monte_carlo.py
+++ test_monte_carlo.py
@@ -1,6 +1,13 @@
 import pytest
 from monte_carlo import MonteCarlo
 
-def test_init_monte_carlo():
 
-    pass
+
+def test_init_monte_carlo(mocker):
+    model = mocker.Mock()
+    theta_distribution = mocker.Mock()
+
+    mc = MonteCarlo(model, theta_distribution)
+
+    assert mc.model == model
+    assert mc.theta_distribution == theta_distribution
